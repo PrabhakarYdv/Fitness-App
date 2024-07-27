c4b0e482cd84ce9428174a7a10a194e43b534335
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-07-27 23:21:36 +0530

    Home Activity UI Done

--- app/src/main/java/com/prabhakar/fitnessapp/HomeActivity.kt
+++ app/src/main/java/com/prabhakar/fitnessapp/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.prabhakar.fitnessapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.prabhakar.fitnessapp.databinding.ActivityHomeBinding
 
@@ -12,4 +13,11 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
     }
+
+    private fun beforeAge18(view: View) {
+
+    }
+
+    private fun afterAge18(view: View) {}
+    private fun goToFood(view: View) {}
 }
