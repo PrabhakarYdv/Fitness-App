e3f9233abb2b9506807b6a0d2eb010ee7e44fa6f
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-07-27 22:52:16 +0530

    Bug Fix

---
+++ app/src/main/java/com/prabhakar/fitnessapp/SplashScreenActivity.kt
@@ -0,0 +1,38 @@
+package com.prabhakar.fitnessapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.prabhakar.fitnessapp.databinding.ActivitySplashScreenBinding
+
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var logoAnimation: Animation
+    private lateinit var textAnimation: Animation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        logoAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_logo_animation)
+        textAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_text_animation)
+        binding.apply {
+            splashLogo.animation = logoAnimation
+            splashText.animation = textAnimation
+        }
+
+        Handler().postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
