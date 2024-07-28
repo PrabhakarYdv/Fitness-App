9b4123f2bb3e20753b93e1610aa4982989677262
PrabhakarYdv <prabhakarkr802@gmail.com>
2024-07-28 14:45:31 +0530

    Count Down TImer Done

--- app/src/main/java/com/prabhakar/fitnessapp/ExerciseActivity.kt
+++ app/src/main/java/com/prabhakar/fitnessapp/ExerciseActivity.kt
@@ -1,8 +1,14 @@
 package com.prabhakar.fitnessapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.prabhakar.fitnessapp.databinding.ActivityExerciseBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
@@ -16,8 +22,37 @@ class ExerciseActivity : AppCompatActivity() {
         binding.apply {
             exerciseName.text = name
             exerciseImage.setImageResource(image)
-            timer.text="0${duration}:00"
+            timer.text = "0${duration}:00"
+
+            btnStart.setOnClickListener {
+                startTimer()
+//                finish()
+                btnStart.visibility = View.GONE
+            }
         }
+
+    }
+
+    private fun startTimer() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val totalSeconds = TimeUnit.MINUTES.toSeconds(1)
+            val tillSecond = 0
+            for (second in totalSeconds downTo tillSecond) {
+                if (second == 0.toLong()) {
+                    finish()
+                }
+                val time = String.format(
+                    "%02d:%02d",
+                    TimeUnit.SECONDS.toMinutes(second),
+                    second - TimeUnit.MINUTES.toSeconds(TimeUnit.SECONDS.toMinutes(second))
+                )
+                binding.timer.text = time
+                delay(1000)
+
+            }
+
+        }
+
     }
 
 }
